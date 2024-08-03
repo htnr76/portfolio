3c041d920facc1b92997b42af0e04fb1f93f95fb
Nikita Reztsov <nikita@fibr.se>
2024-08-03 23:33:00 +0200

    1

--- app/Comps/3d.tsx
+++ app/Comps/3d.tsx
@@ -2,10 +2,10 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Robot() {
     return (
-        <main className='h-full min-h-[350px] max-h-[500px] w-auto bg-inherit'>
-            <Spline
-                scene="https://prod.spline.design/9npGEjWVzU9b8nTh/scene.splinecode"
-            />
+        <main className='h-full min-h-[350px] max-h-[500px] w-auto bg-transparent'>
+           <Spline
+        scene="https://prod.spline.design/9CBD8E0TC8nZ9WC1/scene.splinecode" 
+      />
         </main>
     );
 }
